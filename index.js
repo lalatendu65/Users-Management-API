cf7622c7d9b4a43ef2aa4f475fc87d7238c40e8b
Lalatendu <lalatendupany.2000@gmail.com>
2024-08-22 23:52:47 +0530

    "Added a new root route (/) that returns a welcome message in HTML"

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ connection(URL);
 const router = require("./src/routes/customerroutes");
 app.use(express.json());
 app.use("/api", router);
+app.get("/", (req, res) => {
+  res.send(
+    `<body style = "display: flex; background-color: #89CFF0; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+      <h1 style = "width: 200px; height: 200px; background-color: #89CFF0; text-align: center; padding: 20px;">
+        Welcome to User management Api !
+      </h1>
+    </body>`
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
